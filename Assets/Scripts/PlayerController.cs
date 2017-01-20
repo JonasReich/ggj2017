31cc9daff3b0d1868e19e4806ab0ebb1a57762ec
Dominik Lossie <dolossie@stud.mediadesign.de>
2017-01-20 21:13:27 +0100

    Added: Basic Movement fo one Player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+    [SerializeField]
+    private int m_iPlayerId;
+
 	// Use this for initialization
-	void Start () {
+	void Awake () {
+
+
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
+
+        this.transform.position += new Vector3(Time.deltaTime * Input.GetAxis("HorizontalP" + m_iPlayerId), Time.deltaTime * Input.GetAxis("VerticalP" + m_iPlayerId));
 		
 	}
 }
