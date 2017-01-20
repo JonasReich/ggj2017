a678520367a0abb7f9942c167494b925b65c61fa
Dominik Lossie <dolossie@stud.mediadesign.de>
2017-01-20 22:19:13 +0100

    Added: Spawning of PLayers (little buggy)

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    [SerializeField]
+    private PlayerController[] PlayerArray;
+    [SerializeField]
+    private PlayerController PlayerPrefab;
+
+	// Use this for initialization
+	void Awake () {
+        PlayerArray = new PlayerController[Input.GetJoystickNames().Length];
+
+        for (int i = 0; i < Input.GetJoystickNames().Length; i++)
+        {
+            PlayerController PlayerTMP = Instantiate(PlayerPrefab, GameObject.Find("Players").transform);
+            PlayerTMP.name = "Player" + i;
+            PlayerTMP.SetId(i);
+            PlayerArray[i] = PlayerTMP;
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,23 +7,19 @@ public class PlayerController : MonoBehaviour {
     [SerializeField]
     private int m_iPlayerId;
 
-    private string m_sHorizontalAxisName;
-    private string m_sVerticalAxisName;
-
     // Use this for initialization
     void Awake () {
-
-        m_sHorizontalAxisName = "HorizontalP" + m_iPlayerId;
-        m_sVerticalAxisName = "VerticalP" + m_iPlayerId;
-        Debug.Log(m_sHorizontalAxisName);
-		
-
     }
 	
 	// Update is called once per frame
 	void FixedUpdate () {
 
-        this.transform.position += new Vector3(Time.deltaTime * Input.GetAxis(m_sHorizontalAxisName), Time.deltaTime * -1 * Input.GetAxis(m_sVerticalAxisName));
+        this.transform.position += new Vector3(Time.deltaTime * Input.GetAxis("HorizontalP" + m_iPlayerId), Time.deltaTime * -1 * Input.GetAxis("VerticalP" + m_iPlayerId));
 		
 	}
+
+    public void SetId(int id)
+    {
+        m_iPlayerId = id;
+    }
 }
