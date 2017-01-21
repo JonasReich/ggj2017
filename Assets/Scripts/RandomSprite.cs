021e9fe9d07cb8457f6caf6a2aba0b7c217455da
Jonas Reich <joreich@stud.mediadesign.de>
2017-01-22 00:39:55 +0100

    random sprite

---
+++ Assets/Scripts/RandomSprite.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSprite : MonoBehaviour
+{
+	public Sprite[] sprites;
+	
+	void Awake () {
+		var randomIndex = UnityEngine.Random.Range(0, sprites.Length);
+		GetComponent<SpriteRenderer>().sprite = sprites[randomIndex];
+	}
+}
