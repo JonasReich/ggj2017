4632b4ff78501fafc66ed73b3c1abf5115530c3f
Stefan Held(Home) <stheld@stud.mediadesign.de>
2017-01-22 04:53:29 +0100

    Repair Push

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -39,7 +39,12 @@ public class GameManager : MonoBehaviour {
                 PlayerTMP.name = "Player" + i;
                 PlayerTMP.SetGameManager(this);
 				PlayerTMP.SetLevel(Level);
-                //PlayerTMP.SetPosition(new Vector2(Random.Range(-2.0f, 2.0f), Random.Range(-2.0f, 2.0f)));
+                if (i < 2)
+                PlayerTMP.SetPosition(new Vector2(1+i,2+i));
+                if (i==2)
+                PlayerTMP.SetPosition(new Vector2(i,i));
+                if (i == 3)
+                PlayerTMP.SetPosition(new Vector2(1, i));
                 PlayerArray[i] = PlayerTMP;
                 PlayerTMP.Initialize(i);
         }
